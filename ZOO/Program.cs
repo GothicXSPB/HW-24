5266492a066cb6c8ace0b0ba2c63c84ab97f7929
GothicXSPB <ZZZerggg@mail.ru>
2022-09-19 12:29:28 +0300

    Add project files.

---
+++ ZOO/Program.cs
@@ -0,0 +1,12 @@
+﻿using ZOO;
+
+Ratel ratel = new Ratel();
+Ratel ratel1 = new Ratel();
+Ratel ratel2 = new Ratel();
+ratel.Name = "Медоед";
+ratel1.Name = "Димон";
+ratel2.Name = "Креветочная батарея";
+
+ratel.Play();
+ratel2.DoEat();
+ratel1.DoSound();
---
+++ ZOO/Ratel.cs
@@ -0,0 +1,38 @@
+﻿namespace ZOO
+{
+    public class Ratel
+    {
+        public string Name { get; set; }
+
+        public double AmountOfFoodConsumedPerDay { get;private set; }
+
+        public int Age { get; private set; }
+
+        public string SpeciesName { get; private set; }
+
+        public string Biom { get; private set; }
+
+        public string RequiredAreaPerIndividual { get; private set; }
+
+        public string Food { get; private set; }
+
+        public string Diet { get; private set; }
+
+        public string Sound { get; private set; }      
+
+        public void DoEat()
+        {
+            Console.WriteLine($"{Name}: Я поел!");
+        }
+
+        public void DoSound()
+        {
+            Console.WriteLine($"{Name} *рычит*");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine($"{Name} играет с мячиком");
+        }
+    }
+}
